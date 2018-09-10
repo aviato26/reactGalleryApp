b1ae15c7a05f7e4de7b227bc66d4af1732bd3b5e
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2018-09-09 22:22:33 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Search from './components/search';
 import Nav from './components/nav';
 import Gallery from './components/photocontainer';
 import apiKey from './config.js';
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   constructor(props){
@@ -14,10 +14,12 @@ class App extends Component {
     }
   }
 
-  componentDidMount(){
-    this.searchQuery()
-  }
+// once the components are mounted make api call
+componentDidMount(){
+  this.searchQuery()
+}
 
+// function for making api call for photos
   searchQuery = (data = 'cats,dogs,birds') => {
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${data}&format=json&nojsoncallback=1&per_page=24`)
     .then(res => res.json())
@@ -29,19 +31,44 @@ class App extends Component {
     .catch(err => console.log(err))
   }
 
+  // function for custom searching (the match object has several properties on it including the url params which are put into the searchQuery function)
   searchRoute = ({match}) => {
-    this.searchQuery(match.params.q);
-    return true
-  }
+   this.searchQuery(match.params.q);
+   return true
+ }
+
+/* once the api call is made it is set to our state object which is then passed
+   down to our components via props which they will then re-render */
+
+ home = () => {
+   return(
+     <div className='App'>
+       <h3>For custom search type search in URL bar</h3>
+       <Nav clickLinks={this.searchQuery}/>
+       <Gallery data={this.state.pics}/>
+     </div>
+   )
+ }
+
+ searchLink = () => {
+   return(
+     <div className='App'>
+       <Search searchFn={this.searchQuery} />
+       <Nav clickLinks={this.searchQuery}/>
+       <Gallery data={this.state.pics}/>
+     </div>
+   )
+ }
+
 
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-            <Search searchFn={this.searchQuery}/>
-            <Nav clickLinks={this.searchQuery}/>
-            <Gallery data={this.state.pics}/>
-            <Route path='/search/:q' render={this.searchRoute} />
+        <div className='App'>
+          <Switch>
+            <Route path='/search' component={this.searchLink} />
+            <Route path='/' component={this.home} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
